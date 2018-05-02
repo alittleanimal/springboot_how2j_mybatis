942325e6694626043a26587023245f745f9e23e1
zhongwei.xu <741996133@qq.com>
2018-05-02 18:13:03 +0800

    springboot demo how2j

---
+++ src/main/java/com/how2java/springboot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.how2java.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+
+}
